a1c806edd98fa77798da907e7964aeb3076a78ef
Kushal Ghosh <kushalghosh9899@gmail.com>
2020-08-16 22:01:16 +0530

    Added Home Page(All Post) Functionality

--- client/src/components/screens/home.js
+++ client/src/components/screens/home.js
@@ -1,51 +1,40 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 
 function Home(){
+  const [data,setData] = useState([]);
+  useEffect(()=>{
+    fetch('/allpost',{
+      headers:{
+        "Authorization":"Bearer "+localStorage.getItem("jwt")
+      }
+    }).then(res=>res.json())
+    .then(result=>{
+      setData(result.posts)
+    })
+  },[])
   return ( 
       <div className="home">
-        <div className="card home-card">
-          <h5>Kushal</h5>
-          <div className="card-image">
-            <img src="images/honeycomb.jpg"></img>
-          </div>
-          <div className="card-content" >
-          <i className="material-icons" style={{color:"red"}}>favorite</i>
-          <h5>title</h5>           
-            <p>this is body</p>
-            <input 
-            type="text"
-            placeholder="add a comment"
-            />
-          </div>          
-        </div>
-        <div className="card home-card">
-          <h5>Kushal</h5>
-          <div className="card-image">          
-          </div>
-          <div className="card-content">
-          <i className="material-icons" style={{color:"red"}}>favorite</i>
-          <h5>title</h5>           
-            <p>this is body</p>
-            <input 
-            type="text"
-            placeholder="add a comment"
-            />
-          </div>          
-        </div>
-        <div className="card home-card">
-          <h5>Kushal</h5>
-          <div className="card-image">          
-          </div>
-          <div className="card-content">
-          <i className="material-icons" style={{color:"red"}}>favorite</i>
-          <h5>title</h5>           
-            <p>this is body</p>
-            <input 
-            type="text"
-            placeholder="add a comment"
-            />
-          </div>          
-        </div>
+        {
+          data.map(item=>{
+            return(
+              <div className="card home-card" key={item._id}>
+                <h5>{item.postedby.name}</h5>
+                <div>
+                  <img className="card-image" src={item.photo}></img>
+                </div>
+                <div className="card-content" >
+                <i className="material-icons" style={{color:"red"}}>favorite</i>
+                <h5>{item.title}</h5>           
+            <p>{item.body}</p>
+                  <input 
+                  type="text"
+                  placeholder="add a comment"
+                  />
+                </div>          
+              </div>
+            )
+            })
+        }
       </div>
   );
 }
