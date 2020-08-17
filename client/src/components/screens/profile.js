94deeffe4d569ee69b99b29390434cecb7bf8448
Kushal Ghosh <kushalghosh9899@gmail.com>
2020-08-17 15:21:49 +0530

    Added Profile Page Functionality

--- client/src/components/screens/profile.js
+++ client/src/components/screens/profile.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React,{useEffect,useState,useContext} from 'react';
+import {UserContext} from '../../App';
 
 function Profile(){
+  const {state,dispatch} = useContext(UserContext);
+  const [mypics,setPics] = useState([])
+  useEffect(()=>{
+    fetch('/mypost',{
+      headers:{
+        "Authorization":"Bearer "+localStorage.getItem("jwt")
+      }
+    }).then(res=>res.json())
+    .then(result=>{
+      setPics(result.My_Posts);
+    })
+  },[])
   return ( 
     
     <div>
@@ -9,22 +22,20 @@ function Profile(){
           <div className="profile-pic"></div>
         </div>
       </div>
-      <h4>Kushal Ghosh</h4>
+      <h4>{state?state.name:"loading"}</h4>
       <div className="profile-info">
         <h6>100 Posts</h6>
         <h6>2021 Followers</h6>
         <h6>0 Following</h6>
       </div>
-      <div className="gallery">
-        <div className="post"></div>
-        <div className="post"></div>
-        <div className="post"></div>
-        <div className="post"></div>
-        <div className="post"></div>
-        <div className="post"></div>
-        <div className="post"></div>
-        <div className="post"></div>
-        
+      <div className="gallery" >
+        {
+          mypics.map(item=>{
+            return(
+              <img className="post" src={item.photo} key={item._id}></img> 
+            )
+          })
+        }       
       </div>
     </div>
   );
